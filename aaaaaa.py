e21c3010121c04764f17b467198ca38a235e50f8
nikqime <nikolozqime@gmail.com>
2024-01-08 21:52:38 +0400

    commit

---
+++ aaaaaa.py
@@ -0,0 +1,21 @@
+def add (y, x):
+    return (y+x)
+def divide (y, x):
+    return(y-x)
+def multiply (y, x):
+    return (y*x)
+def subtract (y,x):
+    return (y*x)
+
+print ("Select that operation what u want")
+print("operation 1")
+print ("operation 2")
+print ("operation 3")
+print("operation 4")
+
+while True:
+   choice=input ('1','2','3','4') 
+   if choice in ()
+
+    
+    
